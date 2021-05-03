86c2beab35d92a90a971072a0ed582fcbdea6020
Frosendal <frederik.rosendal@hotmail.com>
2021-05-03 14:53:31 +0200

    Adding gRPC clients to ShareBroker

--- ShareBroker/Startup.cs
+++ ShareBroker/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using ShareBrokerService.SAL;
 
 namespace ShareBroker
@@ -27,6 +21,15 @@ namespace ShareBroker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            
+            services.AddGrpcClient<SalesServiceGrpc.Protos.ISalesService>(client => {
+                client.Address = new System.Uri("https://localhost:5001");
+            })
+            .ConfigurePrimaryHttpMessageHandler(() => {
+                var handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                return handler;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
