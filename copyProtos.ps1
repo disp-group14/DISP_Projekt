013e63106d2e18149ad7ae1f7c3fed6fd4fce6af
roman800 <magnus.roman@hotmail.com>
2021-05-10 13:51:03 +0200

    Docker fucker

--- copyProtos.ps1
+++ copyProtos.ps1
@@ -3,7 +3,6 @@ Copy-Item -Path .\Protos -Destination .\OwnershipService -Force -Recurse
 Copy-Item -Path .\Protos -Destination .\PurchaseService -Force -Recurse
 Copy-Item -Path .\Protos -Destination .\SalesService -Force -Recurse
 Copy-Item -Path .\Protos -Destination .\ShareBroker -Force -Recurse
-Copy-Item -Path .\Protos -Destination .\ShareOwnerControl -Force -Recurse
 Copy-Item -Path .\Protos -Destination .\StockService -Force -Recurse
 Copy-Item -Path .\Protos -Destination .\StockTraderBroker -Force -Recurse
 Copy-Item -Path .\Protos -Destination .\stockweb -Force -Recurse
