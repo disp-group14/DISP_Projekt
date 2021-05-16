1b1871f6839e0c693ff55066f33b448a5b375f7a
Frosendal <frederik.rosendal@hotmail.com>
2021-05-16 18:45:31 +0200

    Implemented PurchaseShare in share broker

--- ShareBroker/SAL/ShareBrokerServiceManager.cs
+++ ShareBroker/SAL/ShareBrokerServiceManager.cs
@@ -6,6 +6,7 @@ using static SalesServiceGrpc.Protos.ISalesService;
 using static PurchaseServiceGrpc.Protos.IPurchaseService;
 using static ShareBrokerServiceGrpc.Protos.IShareBrokerService;
 using PurchaseServiceGrpc.Protos;
+using SharedGrpc.Protos;
 using Google.Protobuf.Collections;
 
 namespace ShareBrokerService.SAL {
@@ -29,12 +30,15 @@ namespace ShareBrokerService.SAL {
             return accPrice;
         }
         public override async Task<OfferResponse> SellShare(OfferRequest request, ServerCallContext context ) {
-            var response = purchaseService.FindMatch(new SaleOffer() {
+            // Find a matching purchase offer in PurchaseService
+            var response = await purchaseService.FindMatchAsync(new SaleOffer() {
                 StockId = request.StockId,
                 Amount = request.Amount,
                 Price = request.Price
             });
+
             if (response.Matches.Count > 0) {
+                // If a match was found, 
                 return new OfferResponse(){
                     Receipt = new OfferReceipt() {
                         StockId = request.StockId,
@@ -43,6 +47,7 @@ namespace ShareBrokerService.SAL {
                     }
                 };
             } else {
+                // If no matches were found, return a recipt
                 return new OfferResponse(){
                     Registration = new OfferRegistration(){
                         Message = "We've succesfully registered your offer. Whenver a buyer matches your offer, a trade will be conducted"
@@ -51,8 +56,29 @@ namespace ShareBrokerService.SAL {
             }
         }
 
-        public override Task<OfferResponse> PurchaseShare(OfferRequest request, ServerCallContext context ) {
-            throw new NotImplementedException("Suck my other ass");
+        public override async Task<OfferResponse> PurchaseShare(OfferRequest request, ServerCallContext context ) {
+            // Find a matching sale offer in salesService
+            var response = await salesService.FindMatchAsync(new SalesServiceGrpc.Protos.PurchaseOffer() {
+                StockId = request.StockId,
+                Amount = request.Amount,
+                Price = request.Price
+            });
+
+            if (response.Matches.Count > 0) {
+                // If a match was found, 
+                return new OfferResponse(){
+                    Receipt = new OfferReceipt() {
+                        StockId = request.StockId,
+                        Amount = response.Matches.Count,
+                        Price = sumMatchesPrice(response.Matches)
+                    }
+                };
+            } // If no matches were found, return a recipt
+                return new OfferResponse(){
+                    Registration = new OfferRegistration(){
+                        Message = "We've succesfully registered your offer. Whenver a seller matches your offer, a trade will be conducted"
+                    }
+                };
         }
     }
 }
