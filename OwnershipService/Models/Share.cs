587e8efc37c81756356d844f0e2cbfeedc10b3b8
Frosendal <frederik.rosendal@hotmail.com>
2021-05-16 17:05:35 +0200

    Added GetShareHolder rpc for OwnershipService

--- OwnershipService/Models/Share.cs
+++ OwnershipService/Models/Share.cs
@@ -2,6 +2,8 @@ namespace OwnershipService.Models
 {
     public class Share : ModelBase
     {
+        public float Price { get; set; } 
+        public int StockId { get; set; }
         public ShareHolder ShareHolder { get; set; }
     }
 }
--- OwnershipService/SAL/OwnershipServiceManager.cs
+++ OwnershipService/SAL/OwnershipServiceManager.cs
@@ -5,6 +5,7 @@ using OwnershipService.Models;
 using OwnershipServiceGrpc.Protos;
 using SharedGrpc.Protos;
 using static OwnershipServiceGrpc.Protos.IOwnershipService;
+using System.Linq;
 
 namespace OwnershipService.SAL
 {
@@ -21,6 +22,26 @@ namespace OwnershipService.SAL
         {
             var shareHolder = await shareholderDataManager.Insert(new ShareHolder() {UserId = request.UserId});
             return new Empty();
+        }
+
+        public override async Task<ShareHolderResponse> GetShareHolder(ShareHolderRequest request, ServerCallContext context) {
+            // Get shareholder from db
+            var shareHolder = await shareholderDataManager.GetOne(holder => holder.UserId == request.UserId);
+            var shareHolderResponse = new ShareHolderResponse() {
+                UserId = shareHolder.UserId
+            };
+            // Convert from dm-model to grpc-model
+            shareHolderResponse.Shares.AddRange(shareHolder.Shares.ConvertAll<SharedGrpc.Protos.Share>(share => {
+                return new SharedGrpc.Protos.Share(){
+                    StockId = share.StockId,
+                    Amount = shareHolder.Shares.Count,
+                    Price = shareHolder.Shares.Aggregate((float)0, (acc, share) => {
+                        return acc + share.Price;
+                    })
+                };
+            }));
+
+            return shareHolderResponse;
         }
     }
 }
